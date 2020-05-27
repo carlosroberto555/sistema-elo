937632c91e88e0916a8b4d5158c4c6535ea60ee7
Hugo Lima <hugooolima@gmail.com>
2020-05-27 01:07:12 -0300

    Apagados arquivos desnecessários

--- src/Main/Routes.tsx
+++ src/Main/Routes.tsx
@@ -4,13 +4,11 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import Inicio from "../Inicio";
 import Clientes from "../Clientes";
 import Perfil from "../Perfil";
-import Clientes2 from "../Clientes2";
 import CasoDetalhes from "../CasoDetalhes";
 import Chat from "../Chat";
 import Page404 from "../Page404";
 import Usuarios from "../Usuarios";
 import UsersDetalhes from "../UsersDetalhes";
-import ClienteDetalhes2 from "../ClienteDetalhes2";
 
 export default () => (
   <Switch>
@@ -19,8 +17,6 @@ export default () => (
     <Route exact path="/clientes" component={Clientes} />
     <Route path="/clientes/:id" component={CasoDetalhes} />
     <Route path="/perfil" component={Perfil} />
-    <Route path="/clientes2/:id" component={ClienteDetalhes2} />
-    <Route path="/clientes2" component={Clientes2} />
     <Route path="/usuarios/:id" component={UsersDetalhes} />
     <Route path="/usuarios" component={Usuarios} />
     <Route path="/404" component={Page404} />
